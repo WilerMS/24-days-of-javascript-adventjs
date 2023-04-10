3da107797cf568cc7f8ac47344edd689d008c198
Wiler Marinez <wmarinez-e@sice-outsourcing.com>
2023-04-10 21:11:05 +0200

    Structure folder

---
+++ 2022/13/index.js
@@ -0,0 +1,9 @@
+export function getFilesToBackup(lastBackup, changes) {
+
+  const cambiados = changes
+    .filter(change => change[1] > lastBackup)
+    .map(change => change[0])
+    .sort((a, b) => a - b)
+
+  return [...new Set(cambiados)]
+}

0e52b0378918dcc86a981b800ecee0964b96210f
WilerMS <wilersoriano@gmail.com>
2023-04-26 01:23:34 +0200

    excercise 8 completed

--- 2022/8/index.js
+++ 2022/8/index.js
@@ -1,8 +1,12 @@
-export function checkPart(part) {
+function checkPart(part) {
   return part
     .split('')
     .some((_, i, self) => {
       const text = self.filter((_, j) => j != i)
       return text.join('') === text.reverse().join('')
     })
+}
+
+module.exports = {
+  checkPart
 }

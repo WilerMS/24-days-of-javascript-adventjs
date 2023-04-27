7cd0ea761c97230a27f4513b71aeef00a96bd8be
WilerMS <wilersoriano@gmail.com>
2023-04-27 09:03:08 +0200

    some exercises of 2021 completed

---
+++ 2021/01/index.js
@@ -0,0 +1,9 @@
+function contarOvejas(ovejas) {
+  return ovejas.filter(({name, color}) => {
+    return color === 'rojo' && name.toLowerCase().includes('n') && name.toLowerCase().includes('a')
+  })
+}
+
+module.exports = {
+  contarOvejas
+}
---
+++ 2021/02/index.js
@@ -0,0 +1,16 @@
+function listGifts(letter) {
+  return letter
+    .split(' ')
+    .filter(Boolean)
+    .reduce((acc, curr) => {
+      if (curr.startsWith('_')) return acc
+      return {
+        ...acc,
+        [curr]: (acc[curr] || 0) + 1
+      }
+    }, {})
+}
+
+module.exports = {
+  listGifts
+}

ac04e19f23589b19a8005ed56447973d954c0de2
WilerMS <wilersoriano@gmail.com>
2023-05-08 01:54:57 +0200

    challenge 05 of 2021 completed

--- 2021/05/index.js
+++ 2021/05/index.js
@@ -1,1 +1,9 @@
+function daysToXmas (date) {
+  const now = new Date('Dec 25, 2021').getTime()
+  const time = new Date(date).getTime()
+  return Math.ceil((now - time) / 3600 / 24 / 1000)
+}
 
+module.exports = {
+  daysToXmas
+}

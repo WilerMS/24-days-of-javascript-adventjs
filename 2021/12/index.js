a62dcddaa5371bd6e02c0df5690e871e798d2bcf
WilerMS <wilersoriano@gmail.com>
2023-05-12 06:28:41 +0200

    ch 12 completed

--- 2021/12/index.js
+++ 2021/12/index.js
@@ -1,6 +1,19 @@
 function getMinJump (obstacles) {
-  // ¡No olvides compartir tu solución en redes!
-  return true
+  const max = Math.max(...obstacles) + 2
+  const path = Array
+    .from({ length: max })
+    .map((_, index) => obstacles.includes(index))
+
+  let currentJump = 1
+  for (let i = 1; i <= max; i++) {
+    const isValidJump = path.every((position, j) => {
+      return (j % i === 0) ? !position : true
+    })
+    currentJump = isValidJump ? i : currentJump
+    if (isValidJump) break
+  }
+
+  return currentJump
 }
 
 module.exports = {

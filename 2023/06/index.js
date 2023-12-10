26b75bccdc7a487c079ed2073f375317ae4233b7
WilerMS <wilersoriano@gmail.com>
2023-12-10 21:48:32 +0100

    ch 6 completed

--- 2023/06/index.js
+++ 2023/06/index.js
@@ -1,10 +1,10 @@
 function maxDistance(movements) {
   let distance = 0
   let free = 0
-  for(const movement of movements) {
-    if(movement === '>') distance++
-    else if(movement === '<') distance--
-    else if(movement === '*') free++
+  for(const mov of movements) {
+    if(mov === '>') distance++
+    else if(mov === '<') distance--
+    else if(mov === '*') free++
   }
 
   return Math.abs(distance) + free

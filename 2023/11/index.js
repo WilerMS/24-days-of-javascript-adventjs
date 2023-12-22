10e6dfd6c48a5d34f032b2bdfa36e55f294838a4
WilerMS <wilersoriano@gmail.com>
2023-12-23 00:00:14 +0100

    ch 11

---
+++ 2023/11/index.js
@@ -0,0 +1,21 @@
+function getIndexsForPalindrome(word) {
+  const reversed = word.split('').reverse().join('')
+  if (reversed === word) return []
+
+  for (let i = 0; i < word.length - 1; i++) {
+    for (let j = 1; j < word.length; j++) {
+      let newWord = word.split('')
+      const temp = newWord[i]
+      newWord[i] = newWord[j]
+      newWord[j] = temp
+
+      const storageWord = [...newWord]
+
+      const reversed = newWord.reverse().join('')
+      if (reversed === storageWord.join('')) return [i, j]
+    }
+  }
+
+  return null
+}
+

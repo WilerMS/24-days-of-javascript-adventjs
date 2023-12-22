80069a86c11a73426ba9db16daac184ebc9ae219
WilerMS <wilersoriano@gmail.com>
2023-12-23 00:00:20 +0100

    ch 12

---
+++ 2023/12/index.js
@@ -0,0 +1,22 @@
+function checkIsValidCopy(original, copy) {
+  const degradations = {
+    'minus': ['#','+',':','.',' '],
+    '#': ['+',':','.',' '],
+    '+': [':','.',' '],
+    ':': ['.',' '],
+    '.': [' '],
+    ' ': []
+  }
+
+  return original.split('').every((letter, i) => {
+    const isUpper = /^[A-Z]$/.test(letter)
+    const isLower = /^[a-z]$/.test(letter)
+        
+    if (letter === copy[i] || (isUpper && letter.toLowerCase() === copy[i])) {
+      return true
+    }
+    
+    const key = !isUpper && !isLower  ? letter : 'minus'
+    return degradations[key].includes(copy[i])
+  })
+}
